f6c004bfa611e979c29932f9f3c061d4db3e34f5
Dylan Wolff <dylanwolff@gmail.com>
2015-04-19 15:03:52 +1000

    Update SendGrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # SendGrid email config
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'limitless-brook-4640.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
